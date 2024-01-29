d0aa9d0a39e378ad0e7f2e84b0ef6546d6f297a0
danishkodemonkey <humberjini@hotmail.com>
2024-01-29 17:03:05 +0100

    Pass javascript exercise 5
    Check if year entered passes specific conditions using statements and operators
    
    all tests passed!

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,14 @@
-const leapYears = function() {
+const leapYears = function (year) {
+  /*
+    pseudoCode
+    Using if statements && operators, check if a given parameter meets the following conditions
+    year is divisible by 4
+    year is NOT divisible by 100
+    years ARE divisible by 400
+    */
 
-};
+  return year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0)
+}
 
 // Do not edit below this line
-module.exports = leapYears;
+module.exports = leapYears

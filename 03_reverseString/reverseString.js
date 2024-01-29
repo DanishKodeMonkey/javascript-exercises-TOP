2e8b9d93c94e33e4d92c951bb0d220e3154e2e76
danishkodemonkey <humberjini@hotmail.com>
2024-01-29 15:34:15 +0100

    Pass javascript exercise 3
    create function to reverse given string, e.g "Hello" becomes "olleH"
    all tests passed!

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,7 @@
-const reverseString = function() {
-
-};
+const reverseString = function (string) {
+  let reverseString = string.split("").reverse().join("")
+  return reverseString
+}
 
 // Do not edit below this line
-module.exports = reverseString;
+module.exports = reverseString

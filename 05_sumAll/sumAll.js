6c32bae090bdca874693fcfe87dbd1f2f8e5bdad
danishkodemonkey <humberjini@hotmail.com>
2024-01-29 16:47:53 +0100

    Pass exercise 5 of javascript exercises
    calculate the sum of all numbers between two given parameters
    check for errors, mainly invalid types
    
    all tests passed!

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,17 @@
-const sumAll = function() {
+const sumAll = function (startNum, endNum) {
+  let highNum = Math.max(startNum, endNum)
+  let lowNum = Math.min(startNum, endNum)
+  let finalSum = 0
 
-};
+  if (highNum < 0 || lowNum < 0) return "ERROR"
+  else if (typeof startNum !== "number" || typeof endNum !== "number")
+    return "ERROR"
 
+  for (i = lowNum; i <= highNum; i++) {
+    finalSum += i
+  }
+
+  return finalSum
+}
 // Do not edit below this line
-module.exports = sumAll;
+module.exports = sumAll
